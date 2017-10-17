21301989611e9329c81b8c8759a98445d25f8fdd
Abel <abel.avila@sycamoreleaf.com>
2017-10-17 15:01:08 -0500

    Fix earnigs view

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171011211241) do
+ActiveRecord::Schema.define(version: 20171016215340) do
 
   create_table "earnings", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
-    t.float    "planned",    limit: 24, default: 0.0
+    t.float    "planned",    limit: 24
     t.float    "actual",     limit: 24
     t.date     "date"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+  end
+
+  create_table "monthly_bills", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.integer  "group_id"
+    t.date     "due_date"
+    t.float    "planned",    limit: 24
+    t.float    "actual",     limit: 24
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
