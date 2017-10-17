4f211c705a47f99f672a6570ddd2f77549734c1f
Abel <abel.avila@sycamoreleaf.com>
2017-10-17 14:59:31 -0500

    Update Earnings Controller

--- app/controllers/earnings_controller.rb
+++ app/controllers/earnings_controller.rb
@@ -1,14 +1,18 @@
 class EarningsController < ApplicationController
   def index
-    @earnings = Earning.all
     @earning = Earning.new
-    @total = Earning.sum(:planned)
+    @today = Date.today.strftime("%m/%d/%Y")
+    @month = params[:month] ? Date.parse(params[:month]) : Date.today
+    month_start = @month.beginning_of_month
+    month_end = @month.end_of_month
+    @earnings = Earning.where(date: month_start..month_end).order(:date)
+    @total = @earnings.sum(:planned)
   end
 
   def create
     @earning = Earning.create(earning_params)
     if @earning.save()
-        redirect_to root_path
+        redirect_to :back
     else 
         redirect_to root_path
     end
@@ -21,12 +25,25 @@ class EarningsController < ApplicationController
     redirect_to root_path
   end
 
+  def update
+    @earning = Earning.find(params[:id])
+    if @earning.update_attributes(earning_params)
+      redirect_to :back
+    else 
+      redirect_to root_path
+    end
+  end
+
   private 
     def earning_params
         values = params.require(:earning).permit(:name, :date, :planned, :actual)
         date = params[:earning][:date]
         date = Date.strptime(date, "%m/%d/%Y") unless date == ""
         values.merge(date: date)
+    end
+
+    def basic_params
+        values = params.require(:earning).permit(:name, :date, :planned, :actual)
     end
     
 end
--- app/models/earning.rb
+++ app/models/earning.rb
@@ -1,7 +1,2 @@
 class Earning < ApplicationRecord
-  after_initialize :init
-
-  def init
-    self.planned ||= 0.00
-  end
 end
